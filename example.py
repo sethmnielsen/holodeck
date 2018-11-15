96cbae136954a922b90420051f75782c3b505cfe
mitchell <maxdrob1@gmail.com>
2018-11-15 15:04:10 -0700

    package manager updates

--- example.py
+++ example.py
@@ -5,96 +5,24 @@ import holodeck
 from holodeck import agents
 from holodeck.environments import *
 from holodeck.sensors import Sensors
-import cv2
-
-
-def uav_example():
-    """A basic example of how to use the UAV agent."""
-    env = holodeck.make("UrbanCity")
-
-    # This changes the control scheme for the uav
-    env.set_control_scheme("uav0", ControlSchemes.UAV_ROLL_PITCH_YAW_RATE_ALT)
-
-    for i in range(10):
-        env.reset()
-
-        # This command tells the UAV to not roll or pitch, but to constantly yaw left at 10m altitude.
-        command = np.array([0, 0, 2, 10])
-        for _ in range(1000):
-            state, reward, terminal, _ = env.step(command)
-
-            # To access specific sensor data:
-            pixels = state[Sensors.PIXEL_CAMERA]
-            velocity = state[Sensors.VELOCITY_SENSOR]
-            # For a full list of sensors the UAV has, view the README
-
-    # It is useful to know that you can control the AgentFollower camera(what you see) by pressing V to toggle spectator
-    # mode. This detaches the camera and allows you to move freely about the world.
-    # You can also press C to snap to the location of the pixel camera to see the world from the perspective of the
-    # agent. See the Controls section of the ReadMe for more details.
-
-
-def sphere_example():
-    """A basic example of how to use the sphere agent."""
-    env = holodeck.make("MazeWorld")
-
-    # This command is to constantly rotate to the right
-    command = 2
-    for i in range(10):
-        env.reset()
-        for _ in range(1000):
-            state, reward, terminal, _ = env.step(command)
-
-            # To access specific sensor data:
-            pixels = state[Sensors.PIXEL_CAMERA]
-            orientation = state[Sensors.ORIENTATION_SENSOR]
-            # For a full list of sensors the sphere robot has, view the README
-
-
-def android_example():
-    """A basic example of how to use the android agent."""
-    env = holodeck.make("AndroidPlayground")
-
-    # The Android's command is a 94 length vector representing torques to be applied at each of his joints
-    command = np.ones(94) * 10
-    for i in range(10):
-        env.reset()
-        for j in range(1000):
-            if j % 50 == 0:
-                command *= -1
-
-            state, reward, terminal, _ = env.step(command)
-
-            # To access specific sensor data:
-            pixels = state[Sensors.PIXEL_CAMERA]
-            orientation = state[Sensors.ORIENTATION_SENSOR]
-            # For a full list of sensors the android has, view the README
-
 
 def multi_agent_example():
     """A basic example of using multiple agents"""
-    env = holodeck.make("UrbanCity")
+    env = holodeck.make("Ocean")
 
     cmd0 = np.array([0, 0, -2, 10])
-    cmd1 = np.array([0, 0, 5, 10])
+    cmd1 = 10
     for i in range(10):
         env.reset()
-        # This will queue up a new agent to spawn into the environment, given that the coordinates are not blocked.
-        sensors = [Sensors.PIXEL_CAMERA, Sensors.LOCATION_SENSOR, Sensors.VELOCITY_SENSOR]
-        agent = AgentDefinition("uav1", agents.UavAgent, sensors)
-        env.spawn_agent(agent, [1, 1, 5])
-
-        env.set_control_scheme("uav0", ControlSchemes.UAV_ROLL_PITCH_YAW_RATE_ALT)
-        env.set_control_scheme("uav1", ControlSchemes.UAV_ROLL_PITCH_YAW_RATE_ALT)
-
-        env.tick()  # Tick the environment once so the second agent spawns before we try to interact with it.
 
         env.act("uav0", cmd0)
-        env.act("uav1", cmd1)
+        env.act("boat0", cmd1)
+
+        env.set_ocean_state(1, 1, 0)
+        # env.set_day_time(0)
+
         for _ in range(1000):
             states = env.tick()
-            uav0_terminal = states["uav0"][Sensors.TERMINAL]
-            uav1_reward = states["uav1"][Sensors.REWARD]
 
 
 def world_command_examples():
@@ -157,63 +85,27 @@ def editor_example():
     ]
 
     env = HolodeckEnvironment(agent_definitions, start_world=False)
-    # env.agents["boat0"].set_control_scheme(1)
     command = np.array([20])
     _ = env.step(command)
     env.set_day_time(8)
 
     wave_intensity, wave_size, wave_direction = 13, 1, 0
-
-    # env.teleport()
-
+    print("hello")
     for i in range(10):
         env.reset()
         # env.set_weather("rain")
         env.set_ocean_state(13, 1, 0)
         env.act("uav0", [0, 0, 0, 0])
-        env.act("boat0", [20])
+        env.act("boat0", 20)
         states = env.tick()
-
-        # cv2.namedWindow("Image")
-        # cv2.moveWindow("Image", 500, 500)
-        # cv2.imshow("Image", states["uav0"][Sensors.PIXEL_CAMERA][:, :, 0:3])
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
-
-        # env.set_day_time(0)
 
         for _ in range(10000):
             states = env.tick()
             print(states["boat0"][Sensors.LOCATION_SENSOR][0])
 
 
-def editor_multi_agent_example():
-    """This editor example shows how to interact with holodeck worlds that have multiple agents.
-    This is specifically for when working with UE4 directly and not a prebuilt binary.
-    """
-    agent_definitions = [
-        AgentDefinition("uav0", agents.UavAgent, [Sensors.PIXEL_CAMERA, Sensors.LOCATION_SENSOR]),
-        AgentDefinition("uav1", agents.UavAgent, [Sensors.LOCATION_SENSOR, Sensors.VELOCITY_SENSOR])
-    ]
-    env = HolodeckEnvironment(agent_definitions, start_world=False)
-
-    cmd0 = np.array([0, 0, -2, 10])
-    cmd1 = np.array([0, 0, 5, 10])
-
-    for i in range(10):
-        env.reset()
-        env.act("uav0", cmd0)
-        env.act("uav1", cmd1)
-        for _ in range(1000):
-            states = env.tick()
-
-            uav0_terminal = states["uav0"][Sensors.TERMINAL]
-            uav1_reward = states["uav1"][Sensors.REWARD]
-
-
 if __name__ == "__main__":
     # if 'DefaultWorlds' not in holodeck.installed_packages():
     #     holodeck.install("DefaultWorlds")
     #     print(holodeck.package_info("DefaultWorlds"))
-
-    editor_example()
+    multi_agent_example()
