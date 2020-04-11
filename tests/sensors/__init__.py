456e3237d6233ac410e51c4a9058d5865efc65e8
Seth Nielsen <sethmnielsen@gmail.com>
2020-04-11 14:32:25 -0600

    Deleted unneccessary stuff

--- tests/sensors/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Tests in this module are intended to be for specific sensors, not for every world in general.
-Ideally, there would be tests for every sensor available in holodeck, using the TestWorld
-
-"""
